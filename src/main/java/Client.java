7f391b22f7fabc633941cf08242c6d82bbe06c98
VolLol <nurs.ypurs@gmail.com>
2020-04-30 18:25:19 +0300

    Add something

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,39 +1,31 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) {
-        int PORT = 2391;
-        Socket socket = null;
-        String host = "localhost";
-        String sendingMessage = "Message from client o fuck";
-        try {
-            socket = new Socket(host, PORT);
-            System.out.println("Successful connect");
 
-            System.out.println("Send to server");
-            OutputStream outputStream = socket.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            bufferedWriter.write(sendingMessage);
-            System.out.println("Sending message: " + sendingMessage);
-            bufferedWriter.flush();
+        String hostName = "localhost";
+        int portNumber = 2000;
 
-
-            InputStream inputStream = socket.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String messageFromServer = bufferedReader.readLine();
-            System.out.println("Message from server :" + messageFromServer);
-
-            inputStream.close();
-            socket.close();
+        try (Socket socket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);//считывает исходящий поток
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//считывает входящий поток
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))//cчитывает строку
+        ) {
+            System.out.println("Client start working. Print you string");
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server answer is: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
         }
-
-
     }
-
-
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,43 +1,72 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Server {
     public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        int PORT = 2391;
-        Socket socketToClient = null;
-        String host = "localhost";
-        try {
-            System.out.println("Server starting");
-            serverSocket = new ServerSocket(PORT);
-            socketToClient = serverSocket.accept();
-            System.out.println("Sucsessful connect");
+        int portNumber = 2000;
+        System.out.println("Server start working");
+        System.out.println("----------------------------------------------");
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber);
+             Socket clientSocket = serverSocket.accept();
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        ) {
+            String inputLine;//что выйдет в строке со стороны сервера и что пришло от польователя
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Client write: " + inputLine);
+                Server server = new Server();
+                server.command(inputLine, out, in);
 
 
-            InputStream inputStream = socketToClient.getInputStream();
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String messageFromClient = bufferedReader.readLine();
-            System.out.println("Message from client is: " + messageFromClient);
-
-
-            OutputStream outputStream = socketToClient.getOutputStream();
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-            BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-            String messageFromServer = "IAMALIVE";
-            bufferedWriter.write(messageFromServer);
-            System.out.println("Send message for client like this:" + messageFromServer);
-            bufferedWriter.flush();
-
-            socketToClient.close();
-            serverSocket.close();
+            }
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e) {
-            e.printStackTrace();
+            System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+            System.out.println(e.getMessage());
         }
     }
 
+    public PrintWriter command(String inputLine, PrintWriter out, BufferedReader in) {
+        if (inputLine.equals("v")) {
+            System.out.print("Client ask about version. Return version for him" + "\n");
+            out.println("version is 123456");
+        } else if (inputLine.equals("txt")) {
+            System.out.print("Server need return txt. Return text from file" + "\n");
+            for (Object s : showFile()) {
+                out.println(s);
+            }
 
+        } else if (inputLine.contains("login") && inputLine.length() > 5) {
+            System.out.print("Server need result of checking login and password" + "\n");
+            String login = "nurs";
+            String password = "123";
+            if (inputLine.contains(login) && inputLine.contains(password)) {
+                out.println("Success");
+            } else out.println("Failed");
+        } else out.println(inputLine);
+        System.out.println("----------------------------------------------");
+
+        return out;
+    }
+
+    private List showFile() {
+        List<String> listOfStringsFromFile = new ArrayList<>();
+        String path = "C:\\Develop\\simple echo server with client\\src\\main\\resources\\file.txt";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                listOfStringsFromFile.add(currentLine);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listOfStringsFromFile;
+    }
 }
