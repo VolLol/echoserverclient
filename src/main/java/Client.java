8702d2c9cd5ad6da46d8dac5a658cb19564dcb4b
VolLol <nurs.ypurs@gmail.com>
2020-05-03 12:24:59 +0300

    Add something

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,31 +1,19 @@
-import java.io.*;
+import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class Client {
+
     public static void main(String[] args) {
-
+        int PORT = 2020;
         String hostName = "localhost";
-        int portNumber = 2002;
-
-        try (Socket socket = new Socket(hostName, portNumber);
-             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);//считывает исходящий поток
-             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//считывает входящий поток
-             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))//cчитывает строку
+        ClientProcessor clientProcessor = new ClientProcessor();
+        System.out.println("Client start work");
+        try (Socket socket = new Socket(hostName, PORT);
         ) {
-            System.out.println("Client start working. Print you string");
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("Server answer: " + in.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
+            System.out.println("Successful connect");
+            clientProcessor.execute(socket);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    hostName);
-            System.exit(1);
+            e.printStackTrace();
         }
     }
 }
---
+++ src/main/java/v01/Client.java
@@ -0,0 +1,33 @@
+package v01;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+    public static void main(String[] args) {
+
+        String hostName = "localhost";
+        int portNumber = 2002;
+
+        try (Socket socket = new Socket(hostName, portNumber);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);//считывает исходящий поток
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//считывает входящий поток
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))//cчитывает строку
+        ) {
+            System.out.println("firstTry.Client start working. Print you string");
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("firstTry.Server answer: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        }
+    }
+}
